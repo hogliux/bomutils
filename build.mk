97909c8e3e00da952b32cc6cf86b50a4d4ccd351
Andrew Savchenko <andrew@savchenko.net>
2021-05-09 16:09:12 +0930

    Update build.mk
    - Strip symbols from the binaries

--- build.mk
+++ build.mk
@@ -7,12 +7,12 @@
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2, or (at your option)
 #  any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
@@ -73,6 +73,7 @@ $(BUILD_OBJ_DIR)/%.d : %.cpp
 $(BUILD_BIN_DIR)/%$(SUFFIX) : $(BUILD_OBJ_DIR)/%.o $(COMMON_OBJECTS)
 	@mkdir -p $(BUILD_BIN_DIR)
 	$(CXX) -o $@ $(LDFLAGS) $^ $(LIBS)
+	@strip --strip-unneeded $@
 
 $(BUILD_MAN_DIR)/%.1.gz : %.1
 	@mkdir -p $(BUILD_MAN_DIR)
